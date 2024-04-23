7770ce821157d7f809f7549a8fbc72ae1c881dfe
sainath-07 <sainathb308@gmail.com>
2024-04-24 05:18:56 +0530

    update

--- src/23-04-2024/Component/button.js
+++ src/23-04-2024/Component/button.js
@@ -1,6 +1,26 @@
+
+// Shorcut  to write the Arrow function if it is returning the single line statement
+
+
+// const Buttoncomponent1 = () => <button>button</button>
+ 
+
 const Buttoncomponent1= () => {
-    return (
-           <button>button</button>
-    )
+   const  b=0
+    
+    if(b){
+        return (
+            <button> if button</button>
+        )
+
+    }
+    else{
+        return (
+            <button> else button</button>
+        )
+    }
 }
+
+
+
 export default Buttoncomponent1
