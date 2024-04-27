bef5381a007283c6b39e95651eeefb3255e655f6
Sainath <118004526+sainath-07@users.noreply.github.com>
2024-04-27 20:23:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,3 @@
-// import Createbutton from "./component/button/button.js"
-// import Imagecomponent from "./component/image/image.js"
-// import Tablecomponent from "./component/tables/tables.js"
-// import Formcomponent from "./component/form/form.js"
-// import ListComponent from "./component/list/list-component.js"
-// import Orderedlist from "./component/list/orderlist.js"
-
-
-// 23-04-2024
-
-// import Buttoncomponent1 from "./23-04-2024/Component/conditionalrednering/button"
-// import ListComponent from "./23-04-2024/Component/conditionalrednering/list"
-// import Arrow from "./23-04-2024/Component/Arrowfunction/arrow"
-// import Iplteamlist from "./23-04-2024/Component/Mapmethod/list"
-// import Tataiplteamlist from "./23-04-2024/Component/Task/task"
-
-
 //24-04-2024
 // import Iplcomponent from "./24-04-2024/components/filter/filter"
 // import Iplcomponent1 from "./24-04-2024/components/map ,filter,reduce/breakdown"
@@ -44,57 +27,7 @@ const Ui = () => {
       {/* <Iplcomponent1/> */}
       {/* <Iplcomponent/> */}
 
-
-
-      {/* 23-04-2024 */}
-
-
-      {/* <Tataiplteamlist/>  */}
-
-      {/* <Iplteamlist/> */}
-      {/* <Arrow/> */}
-      {/* <Buttoncomponent1/> 
-      <ListComponent/>
-
-
-      {/* day02 */}
-      {/* <ListComponent/>
-         <Orderedlist/> */}
-
-
-
-      {/* day 01 */}
-      {/* <h1>Hello world</h1> */}
-      {/* <Createbutton/>
-      <Imagecomponent/> */}
-      {/* <Createbutton/>
-      <Createbutton/>
-      <Createbutton/> */}
-      {/* <div>
-
-      <Imagecomponent/>
-      <Imagecomponent/>
-      <Imagecomponent/>
-      <Imagecomponent/>
-
-       </div> */}
-
-
-      {/* <div>
-      <Tablecomponent/>
-      </div> */}
-
-      {/* <div>
-        <Formcomponent/>
-      </div> */}
-
-
-
-
-    </div>
-
-
-
+ </div>
 
   )
 }
