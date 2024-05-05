17c453a385ab9ed5fc047b9aee301e03d27c7200
sainath-07 <sainathb308@gmail.com>
2024-05-05 20:39:03 +0530

    state_hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import EventHandler from "./ReactCourse/Eventhandling/Eventhadler"
-import Statehook from "./ReactCourse/Hook/useState/statehook"
-import Demo from "./ReactCourse/state/stateusingtheeventhandlers"
 import Statehookarray from "./ReactCourse/Hook/useState/statehookarray"
 
 
