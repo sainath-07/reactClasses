a355d2c6c60b7fa44ae73c509d2dc050af479959
sainath-07 <sainathb308@gmail.com>
2024-05-05 20:37:17 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import EventHandler from "./ReactCourse/Eventhandling/Eventhadler"
-import Statehook from "./ReactCourse/Hook/statehook"
+import Statehook from "./ReactCourse/Hook/useState/statehook"
 import Demo from "./ReactCourse/state/stateusingtheeventhandlers"
-import Statehookarray from "./ReactCourse/Hook/statehookarray"
+import Statehookarray from "./ReactCourse/Hook/useState/statehookarray"
 
 
 
