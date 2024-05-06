018416a3ae425a8e871dd19b2ed1ce7dfa87eee2
sainath-07 <sainathb308@gmail.com>
2024-05-06 15:55:29 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@ import Contact from "./ReactCourse/Routing/Contact"
 import Home from "./ReactCourse/Routing/Home"
 import Login from "./ReactCourse/Routing/Login"
 import Services from "./ReactCourse/Routing/services"
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 
 
+
+const Header=()=>{
+  return(
+    <>
+         <ul>
+           <li> <Link to="/">Home</Link> </li>
+           <li> <Link to="/service">service</Link> </li>
+           <li> <Link to="/login">login</Link> </li>
+           <li> <Link to="/Contact">Contact</Link> </li>
+         </ul>
+    </>
+  )
+}
 
 const Ui = () => {
 
  return(
-  <BrowserRouter></BrowserRouter>
+  <>
+  <div>div </div>
+  <BrowserRouter>
+     <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+       <Route path="/service" element={<Services/>} />
+       <Route path="/login" element={<Login/>} />
+       <Route path="/Contact" element={<Contact/>} />
+    </Routes>
+  </BrowserRouter>
+  </>
 
     //  <>
     //  <Home/>
