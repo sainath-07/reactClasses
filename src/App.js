56f53cbc5934dc888802ab626180aa1556444cc3
sainath-07 <sainathb308@gmail.com>
2024-05-19 16:51:08 +0530

    Functional component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import Home from "./ReactCourse/Functional component hooks/usenavigation & useLo
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import About from "./ReactCourse/Functional component hooks/usenavigation & useLocation/about";
 import Child from "./ReactCourse/Functional component hooks/dummy/child";
+import Parent from "./ReactCourse/Functional component hooks/dummy/parent";
 
 const Render=()=>{
    return(
     <>
-    {/* <BrowserRouter>
+    <BrowserRouter>
     
     <Routes>
       <Route path="/" Component={Home}></Route>
       <Route path="/about" Component={About}></Route>
     </Routes>
-    </BrowserRouter> */}
+    </BrowserRouter>
 
-    <Child/>
     </>
    )
 }
