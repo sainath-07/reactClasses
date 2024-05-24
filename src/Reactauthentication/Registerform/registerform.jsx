9d219938f7432a391c344ff80d25110c9575ebe6
sainath-07 <sainathb308@gmail.com>
2024-05-24 18:05:17 +0530

    add

--- src/Reactauthentication/Registerform/registerform.jsx
+++ src/Reactauthentication/Registerform/registerform.jsx
@@ -1,121 +1,127 @@
 import { Toast } from "bootstrap";
-import { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, json, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { eachinput } from "../utillis/eachinput";
+import axios from 'axios';
 
 const RegisterForm = () => {
 
+//   useEffect(()=>{
+//     fetchdb()
+//   },[])
 
+
+
+
+
+
+// const fetchdb=async()=>{
+//   let res=await axios.get("http://localhost:3000/posts")
+//  console.log(res)
+      
+// }
+
+
+
+
+
+
+  const [emailvalidate, setemailvalidate] = useState([]);
   // useState, passed the state to each and every input element in the form
 
   const [username, setusername] = useState("");
   const [Country, setCountry] = useState("India");
   const [gender, setgender] = useState("");
   const [Address, setAddress] = useState("");
-  
-  
+
   // Navigate to login page.......
   const navigate = useNavigate();
-  
-  
-  
-    // password input field.
-    const [Emailid, setEmailid] = useState("");
+
+  // password input field.
+  const [Emailid, setEmailid] = useState("");
   // validating the password
   // boolean value
-  const [Emailvalue,setEmailvalue]=useState(null)
-  const[Emailerror,setEmailerror]=useState(null)
-  const onchangeemail=(event)=>{
-    let check=event.target.value
-    setEmailid(check)
+  const [Emailvalue, setEmailvalue] = useState(null);
+  const [Emailerror, setEmailerror] = useState(null);
+  const onchangeemail = (event) => {
+    let check = event.target.value;
+    setEmailid(check);
 
-    if(check.length<6 || (!check.includes('@')) ) {
-      setEmailerror("Enter the valid Email id.")
-      setEmailvalue(true)
+    if (check.length < 6 || !check.includes("@")) {
+      setEmailerror("Enter the valid Email id.");
+      setEmailvalue(true);
+    } else {
+      setEmailerror("");
+      setEmailvalue(false);
     }
-    else{
-      setEmailerror("")
-      setEmailvalue(false)
-    }
+  };
 
-       
-  }
-
-
-
-    // password input field.
-    const [Password, setPassword] = useState(""); 
+  // password input field.
+  const [Password, setPassword] = useState("");
   // validating the password
   // boolean value
-  const [value,setvalue]=useState(null)
-  const[showerror,setshowerror]=useState(null)
-  const onchangepassword=(event)=>{
-    let check=event.target.value
-    setPassword(check)
+  const [value, setvalue] = useState(null);
+  const [showerror, setshowerror] = useState(null);
+  const onchangepassword = (event) => {
+    let check = event.target.value;
+    setPassword(check);
 
-    if(check.length>12 || check.length<=6 ) {
-      setshowerror("Password must be between 6 to 12 characters.")
-      setvalue(true)
+    if (check.length > 12 || check.length <= 6) {
+      setshowerror("Password must be between 6 to 12 characters.");
+      setvalue(true);
+    } else {
+      setshowerror("");
+      setvalue(false);
     }
-    else{
-      setshowerror("")
-      setvalue(false)
-    }
-
-       
-  }
-
+  };
 
   // Phone number input field
   const [PhoneNumber, setPhoneNumber] = useState("");
   // boolean value
-  const [phvalue,setphvalue]=useState(null)
-  const[showpherror,setshowpherror]=useState(null)
- const onchangephoneNumber=(event)=>{
-  let check=event.target.value
-  setPhoneNumber(check)
+  const [phvalue, setphvalue] = useState(null);
+  const [showpherror, setshowpherror] = useState(null);
+  const onchangephoneNumber = (event) => {
+    let check = event.target.value;
+    setPhoneNumber(check);
 
-  if(check.length!==10 ){
-    setphvalue(true)
-    setshowpherror("Phone Number must contain only 10 digits")
-  }
-  else{
-    setphvalue(false)
-    setshowpherror("")
+    if (check.length !== 10) {
+      setphvalue(true);
+      setshowpherror("Phone Number must contain only 10 digits");
+    } else {
+      setphvalue(false);
+      setshowpherror("");
+    }
+  };
 
-  }
-     
- }
+  const Isvalidate = () => {
+    let isproceed = true;
+    let errormessage = "please fill input fields in form";
 
- const Isvalidate=()=>{
-  let isproceed=true
-  let errormessage="please fill input fields in form"
+    if (
+      Emailid == "" ||
+      Emailid == null ||
+      PhoneNumber == "" ||
+      PhoneNumber == null ||
+      username == "" ||
+      username == null ||
+      PhoneNumber == "" ||
+      PhoneNumber == null
+    ) {
+      isproceed = false;
+    }
 
-  if(
-    Emailid=="" || Emailid==null ||
-    PhoneNumber=="" || PhoneNumber==null ||
-    username=="" || username==null ||
-    PhoneNumber=="" || PhoneNumber==null 
+    if (!isproceed) {
+      toast.error(errormessage);
+    }
 
-  ){
-    isproceed=false
-  }
-
-  if(!isproceed){
-    toast.error(errormessage);
-  }
-
-  return isproceed
- }
-
+    return isproceed;
+  };
 
   const handleform = (e) => {
     e.preventDefault();
 
-    if(Isvalidate()){
-
-  
+    // if (Isvalidate()) {
       let userdetails = {
         Emailid,
         Password,
@@ -128,26 +134,28 @@ const RegisterForm = () => {
 
       console.log(userdetails);
 
-      fetch("http://localhost:8000/users", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(userdetails),
+     
+   
+
+    fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userdetails),
+    })
+      .then((success) => {
+        console.log(success);
+        toast.success("registered successfully");
+        navigate("/login");
       })
-        .then((success) => {
-          console.log(success);
-          toast.success("registered successfully");
-          navigate("/login");
-        })
-        .catch((err) => {
-          console.log(err);
-          toast.error("singup faild", err);
-        });
-
-      }
+      .catch((err) => {
+        console.log(err);
+        toast.error("singup faild", err);
+      });
   };
+// }
 
 
-  
+
 
   return (
     <>
@@ -167,18 +175,15 @@ const RegisterForm = () => {
                       Email id <span className="text-danger">*</span>
                     </label>
                     <input
-                      
                       type="text"
                       className="form-control"
                       id="email"
                       onChange={onchangeemail}
                       value={Emailid}
-                     
                     />
-                    {
-                      Emailvalue && <span style={{color:"red"}}>{Emailerror}</span>
-                    }
-                    
+                    {Emailvalue && (
+                      <span style={{ color: "red" }}>{Emailerror}</span>
+                    )}
                   </div>
                 </div>
 
@@ -188,18 +193,14 @@ const RegisterForm = () => {
                       Password <span className="text-danger">*</span>
                     </label>
                     <input
-                       
                       value={Password}
                       onChange={onchangepassword}
                       type="Password"
                       className="form-control"
                       id="Password"
                     />
-                   {
-                     value && <span style={{color:"red"}}>{showerror}</span>
-                    }
+                    {value && <span style={{ color: "red" }}>{showerror}</span>}
                   </div>
-                    
                 </div>
 
                 <div className="col-lg-6">
@@ -208,7 +209,6 @@ const RegisterForm = () => {
                       user Name <span className="text-danger">*</span>
                     </label>
                     <input
-                       
                       type="text"
                       value={username}
                       onChange={(e) => setusername(e.target.value)}
@@ -226,7 +226,6 @@ const RegisterForm = () => {
                     </label>
 
                     <input
-                       
                       type="tel"
                       id="PhoneNumber"
                       className="form-control"
@@ -234,9 +233,9 @@ const RegisterForm = () => {
                       onChange={onchangephoneNumber}
                     ></input>
 
-                    {
-                      phvalue && <span style={{color:"red"}}>{showpherror}</span>
-                    }
+                    {phvalue && (
+                      <span style={{ color: "red" }}>{showpherror}</span>
+                    )}
                   </div>
                 </div>
 
@@ -246,12 +245,11 @@ const RegisterForm = () => {
                       Country <span className="text-danger">*</span>
                     </label>
                     <select
-                       
                       className="form-select mt-2"
                       value={Country}
                       onChange={(e) => setCountry(e.target.value)}
                     >
-                      <option  value="India">India</option>
+                      <option value="India">India</option>
                       <option value="Nepal">Nepal</option>
                       <option value="Srilanka">Srilanka</option>
                     </select>
@@ -266,7 +264,6 @@ const RegisterForm = () => {
 
                     <br />
                     <input
-                       
                       className="mt-3"
                       type="radio"
                       id="male"
@@ -288,7 +285,7 @@ const RegisterForm = () => {
                   </div>
                 </div>
 
-                <div className="col-lg-12 mt-2">
+                {/* <div className="col-lg-12 mt-2">
                   <div className="form-group">
                     <label>
                       Address <span className="text-danger">*</span>{" "}
@@ -303,34 +300,39 @@ const RegisterForm = () => {
                         id="floatingTextarea2"
                         style={{ height: 100, resize: "none" }}
                       />
-                      <label className="mt-1 p-0 text-sm text-wrap" htmlFor="floatingTextarea2">
+                      <label
+                        className="mt-1 p-0 text-sm text-wrap"
+                        htmlFor="floatingTextarea2"
+                      >
                         eg : 19/2/3/A,Area,street,city,state.
                       </label>
                     </div>
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
             <div className=" d-flex justify-content-center">
               <div
               //  className="border border-success"
-               >
-
-              <button className="btn btn-success mb-3">Register</button> &nbsp;             
+              >
+                <button className="btn btn-success mb-3">Register</button>{" "}
+                &nbsp;
               </div>
               {/* <div className="border bg-dark text-white">
                 <Link className="text-decoration-none text-white px-1">loginpage</Link>           
               </div> */}
             </div>
-            
           </div>
         </form>
 
         <div className="mt-3 d-flex justify-content-center">
-            {/* <button>  </button> */}
+          {/* <button>  </button> */}
 
-            <button className="btn btn-primary "  type="submit"><Link className="text-white text-decoration-none" to={'/login'}>Login</Link></button>
-
+          <button className="btn btn-primary " type="submit">
+            <Link className="text-white text-decoration-none" to={"/login"}>
+              Login
+            </Link>
+          </button>
         </div>
       </div>
     </>
