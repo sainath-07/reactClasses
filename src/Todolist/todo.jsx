df1376a06c6133a1622434b47987d59ea979a942
sainath-07 <sainathb308@gmail.com>
2024-05-25 18:39:40 +0530

    todolist

--- src/Todolist/todo.jsx
+++ src/Todolist/todo.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import { GrAdd } from "react-icons/gr";
+import { MdDelete } from "react-icons/md";
 
 const Todolist = () => {
   const [text, settext] = useState("");
   const [list, setlist] = useState([]);
   const [duplist, setduplist] = useState([]);
   const [value, setvalue] = useState(null);
+
+  const [updatecontent, setupdatecontent] = useState(true);
 
   const clearall = () => {
     setlist([]);
@@ -17,20 +21,18 @@ const Todolist = () => {
       setvalue(false);
       settext("");
     } else {
+      
       setvalue(true);
     }
   };
 
   console.log(list);
 
-
-
-  const deleteeachtodo=(value)=>{
-    const filterdeleteeachtodo=list.filter((eachtodo,i)=>i!==value)
-    console.log(filterdeleteeachtodo)
-      setlist(filterdeleteeachtodo)
-
-  }
+  const deleteeachtodo = (value) => {
+    const filterdeleteeachtodo = list.filter((eachtodo, i) => i !== value);
+    console.log(filterdeleteeachtodo);
+    setlist(filterdeleteeachtodo);
+  };
 
   return (
     <>
@@ -42,9 +44,24 @@ const Todolist = () => {
           value={text}
           onChange={(e) => settext(e.target.value)}
         />
+        {
+          updatecontent ?
 
-        <button type="button" className="btn btn-success  mx-2" onClick={addtask}>Add ToDo</button>
-  <button type="button" className="btn btn-warning" onClick={clearall}> Clear ToDoList</button>
+        <button
+          type="button"
+          className="btn btn-success  mx-2"
+          onClick={addtask}
+        >
+          Add ToDo
+        </button>
+
+        :
+        <GrAdd />
+        }
+        <button type="button" className="btn btn-warning" onClick={clearall}>
+          {" "}
+          Clear ToDoList
+        </button>
 
         {list.map((eachlist, index) => {
           return (
@@ -52,12 +69,14 @@ const Todolist = () => {
               <div className="container w-75 d-flex flex-wrap  justify-content-between border border-danger h-100  mt-2">
                 <p contentEditable="true">{eachlist}</p>
                 <div className="d-flex gap-2 align-items-center">
-                  <button type="button"  className="btn btn-primary ">
+                  {/* <button type="button"  className="btn btn-primary ">
                     Update
-                  </button>
-                  <button type="button" className="btn btn-danger" onClick={()=>deleteeachtodo(index)}>
+                  </button> */}
+                  <GrAdd />
+                  <MdDelete onClick={() => deleteeachtodo(index)} />
+                  {/* <button type="button" className="btn btn-danger" onClick={()=>deleteeachtodo(index)}>
                     Delete
-                  </button>
+                  </button> */}
                 </div>
               </div>
             </React.Fragment>
